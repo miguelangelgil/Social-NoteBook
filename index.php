77a518dda6113efc5ba0fda2a7c6fc518f86920e
MAGMa <36223494+miguelangelgil@users.noreply.github.com>
2019-08-22 14:03:26 +0200

    licenses

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 include_once 'includes/header.php';
 include_once 'includes/signin_nav_bar.php';
 include_once 'view/new_user.php';
+include_once 'includes/license.php';
 include_once 'includes/footer.php';
 
 
